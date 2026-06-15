cd4283ef4719d49b38ec306d7478e6595b68216e
Andrii <andrii.lavreniuk@oa.edu.ua>
2026-06-15 21:38:33 +0300

    fix: small fix to chat

--- src/components/chat/ContractChatModal.tsx
+++ src/components/chat/ContractChatModal.tsx
@@ -118,7 +118,7 @@ const ContractChatModal: React.FC<ContractChatWidgetProps> = ({
       // Return focus to open button when closing
       openButtonRef.current?.focus();
     }
-  }, [messagesLoading, isOpen, detailsLoading]);
+  }, [messagesLoading, isOpen, detailsLoading, isConnecting]);
 
   // Force fetch fresh data when chat opens so we get correct online status
   useEffect(() => {
